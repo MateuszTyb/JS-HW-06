67c202f1b6541906e4fd09d8ebaac0cf0b415c80
MateuszTyb <86377061+MateuszTyb@users.noreply.github.com>
2022-02-19 09:38:25 +0100

    Poprawa zadania 9 Dzaiałający kod

--- JS-Homework-06/JS/task-09.js
+++ JS-Homework-06/JS/task-09.js
@@ -2,12 +2,15 @@ const randomColor = () =>  {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-let randomRGB = randomColor();
 
 const changeColorBtn = document.querySelector(".change-color");
 const backgroundColor = document.body;
 const colorSubscribe = document.querySelector(".color");
 
-backgroundColor.style.backgroundColor = randomRGB;
-colorSubscribe.textContent = `${randomRGB}`;
-changeColorBtn.addEventListener("click", randomColor);
+colorSubscribe.textContent = `${randomColor()}`;
+changeColorBtn.addEventListener(
+  "click",
+  () => {
+    backgroundColor.style.backgroundColor = colorSubscribe.textContent;
+  colorSubscribe.textContent = `${randomColor()}`;}
+);
