b1d9cf705c510194ee5e381d3e2085126a9e427d
MateuszTyb <86377061+MateuszTyb@users.noreply.github.com>
2022-03-01 22:18:36 +0100

    sdf

--- JS-Homework-06/JS/task-08.js
+++ JS-Homework-06/JS/task-08.js
@@ -1,9 +1,19 @@
-const form = document.querySelector(".register-form");
+const form = document.querySelector(".login-form");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   const {
-    elements: { username, password },
+    elements: { email, password },
   } = event.currentTarget;
-  console.log(username.value, password.value);
+  console.log(email.value, password.value);
+  if (password.value.length < 6) {
+    alert("Twoje hasło musi posiadać przynajmniej 6 znaków");
+  }
+
+  const validRegex =
+    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  if (email.value.match(validRegex)) {
+  } else {
+    alert("Błędny adres email !");
+  }
 });
